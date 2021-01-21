2a2ba412d13dc34d7922be1a43fa0be4e5b7e15b
Caio Rocha <54248474+kayoroxa@users.noreply.github.com>
2021-01-21 14:49:20 -0300

    feact: css base line

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import '../styles/reset.css'
 import type { AppProps } from 'next/app'
 import { StoreProvider } from 'easy-peasy'
 import store from '../store/index.store'
+import { CssBaseline } from '@material-ui/core'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const contextProps = {}
   return (
     <StoreProvider store={store}>
+      <CssBaseline />
       <Component {...pageProps} {...contextProps} />
     </StoreProvider>
   )
