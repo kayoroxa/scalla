9f8c10a022991f1d16e165c6a65f6b7ac30f122c
caio <kayoroxa@gmai.com>
2021-02-13 14:59:52 -0300

    fix: remove import desnecessario

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from '../theme'
-import { StoreProvider, useStoreRehydrated } from 'easy-peasy'
+import { StoreProvider } from 'easy-peasy'
 import store from 'src/store/index.store'
 
 export default function MyApp(props: any) {
