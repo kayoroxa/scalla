925c70653c0ef9e76ff38d9ffb8737e779339589
caio <kayoroxa@gmai.com>
2021-02-22 22:27:45 -0300

    feact: add + backend

---
+++ src/pages/api/habits/[email]/delete/[index].ts
@@ -0,0 +1,33 @@
+import { Db } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from 'src/utils/mongodb'
+
+export default async function habitsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { method } = req
+    switch (method) {
+      case 'POST':
+        const { email, index } = req.query
+        const { db }: { db: Db } = await connectToDatabase()
+
+        await db.collection('user').updateOne(
+          { email },
+          {
+            $pull: {
+              habits: index,
+            },
+          }
+        )
+        res.status(200).json({ message: 'ok' })
+        return
+      default:
+        res.setHeader('Allow', ['GET', 'POST'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: error.message })
+  }
+}
---
+++ src/pages/api/habits/[email]/index.ts
@@ -0,0 +1,25 @@
+import { Db } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from 'src/utils/mongodb'
+
+export default async function habitsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { method } = req
+    switch (method) {
+      case 'GET':
+        const { email } = req.query
+        const { db }: { db: Db } = await connectToDatabase()
+        const data = await db.collection('user').findOne({ email })
+        res.status(200).json(data.habits)
+        return
+      default:
+        res.setHeader('Allow', ['GET', 'PUT'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: error.message })
+  }
+}
