2c860fdcd568764366cbfebe3491e134bf0b5d3b
caio <kayoroxa@gmai.com>
2021-02-23 12:29:31 -0300

    fix: useSWR remove NEXT_PUBLIC_URL

--- src/utils/useSWR.ts
+++ src/utils/useSWR.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import useSWR from 'swr'
-const { NEXT_PUBLIC_URL } = process.env
+// const { NEXT_PUBLIC_URL } = process.env
 const email = 'kayoroxa@gmail.com'
 
 export function useHabit() {
   const fetcher = (url: string) => axios.get(url).then(res => res.data)
-  return useSWR(`${NEXT_PUBLIC_URL}/api/habits/${email}`, fetcher, {
-    refreshInterval: 70000,
+  return useSWR(`/api/habits/${email}`, fetcher, {
+    refreshInterval: 50000,
   })
 }
