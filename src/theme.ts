8c47676acc0395a35eeb31eb418da2c097278ead
caio <kayoroxa@gmai.com>
2021-03-01 14:20:51 -0300

    better color in theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,17 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import { red } from '@material-ui/core/colors'
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#0288d1',
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
+    // secondary: {
+    //   main: '#19857b',
+    // },
+    // error: {
+    //   main: red.A400,
+    // },
     type: 'dark',
   },
 })
